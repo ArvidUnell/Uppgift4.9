d25d3d40079b3d88f8a10bd212dc9678350b89b7
arun06001 <arun06001@E-PW0234KE>
2023-10-20 10:05:55 +0200

    Add project files.

---
+++ Uppgift4.9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en sidlängd på en triangel");
+            short längd = short.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= längd; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine('*');
+            }
+
+            Console.WriteLine("Tryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
